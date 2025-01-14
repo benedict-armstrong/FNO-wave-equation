7f345181d005df470bb3e9c4357868435a6cdbf9
Benedict Armstrong <contact@benarmstro.ng>
2025-01-14 11:45:15 +0100

    initial

---
+++ src/layers.py
@@ -0,0 +1,125 @@
+import torch
+import torch.nn as nn
+
+
+class FILM(torch.nn.Module):
+    def __init__(self, channels, use_bn=True):
+        super(FILM, self).__init__()
+        self.channels = channels
+
+        self.inp2scale = nn.Linear(in_features=1, out_features=channels, bias=True)
+        self.inp2bias = nn.Linear(in_features=1, out_features=channels, bias=True)
+
+        self.inp2scale.weight.data.fill_(0)
+        self.inp2scale.bias.data.fill_(1)
+        self.inp2bias.weight.data.fill_(0)
+        self.inp2bias.bias.data.fill_(0)
+
+        if use_bn:
+            self.norm = nn.BatchNorm1d(channels)
+        else:
+            self.norm = nn.Identity()
+
+    def forward(self, x: torch.Tensor, time: torch.Tensor):
+        x = self.norm(x)
+        time = time.reshape(-1, 1).type_as(x)
+        scale = self.inp2scale(time)
+        bias = self.inp2bias(time)
+        scale = scale.unsqueeze(2).expand_as(x)
+        bias = bias.unsqueeze(2).expand_as(x)
+
+        return x * scale + bias
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, channels, use_bn=True):
+        super(ResidualBlock, self).__init__()
+
+        self.channels = channels
+
+        self.convolution1 = torch.nn.Conv1d(
+            in_channels=self.channels,
+            out_channels=self.channels,
+            kernel_size=5,
+            padding=2,
+            padding_mode="circular",
+        )
+        self.convolution2 = torch.nn.Conv1d(
+            in_channels=self.channels,
+            out_channels=self.channels,
+            kernel_size=11,
+            padding=5,
+            padding_mode="circular",
+        )
+
+        self.batch_norm1 = FILM(self.channels, use_bn)
+        self.batch_norm2 = FILM(self.channels, use_bn)
+        self.batch_norm3 = FILM(self.channels, use_bn)
+
+        # self.dropout1 = nn.Dropout(0.1)
+        # self.dropout2 = nn.Dropout(0.1)
+
+        self.act = torch.nn.ReLU()
+
+    def forward(self, x: torch.Tensor, time: torch.Tensor):
+        out = self.convolution1(x)
+        out = self.batch_norm1(out, time)
+        out = self.act(out)
+        # out = self.dropout1(out)
+
+        out = self.convolution2(out)
+        out = self.batch_norm2(out, time)
+        out = self.act(out)
+        # out = self.dropout2(out)
+
+        # return self.batch_norm3(x, time) + out
+        return x + out
+
+
+class SpectralConv1d(nn.Module):
+    def __init__(self, in_channels, out_channels, modes, use_bn=True):
+        super(SpectralConv1d, self).__init__()
+        """
+        1D Fourier layer. It does FFT, linear transform, and Inverse FFT.
+        """
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.modes1 = modes
+        self.scale = 1 / (in_channels * out_channels)
+        self.weights1 = nn.Parameter(
+            self.scale
+            * torch.rand(in_channels, out_channels, self.modes1, dtype=torch.cfloat)
+        )
+
+        self.batch_norm1 = FILM(self.in_channels, use_bn)
+
+    # Complex multiplication
+    def compl_mul1d(self, input, weights):
+        # (batch, in_channel, x ), (in_channel, out_channel, x) -> (batch, out_channel, x)
+        return torch.einsum("bix,iox->box", input, weights)
+
+    def forward(self, x, time_delta):
+        batchsize = x.shape[0]
+        # x.shape == [batch_size, in_channels, number of grid points]
+        # hint: use torch.fft library torch.fft.rfft
+        # use DFT to approximate the fourier transform
+        # Compute Fourier coefficients
+        x_ft = torch.fft.rfft(x)
+        # Multiply relevant Fourier modes
+        out_ft = torch.zeros(
+            batchsize,
+            self.out_channels,
+            x.size(-1) // 2 + 1,
+            device=x.device,
+            dtype=torch.cfloat,
+        )
+        out_ft[:, :, : self.modes1] = self.compl_mul1d(
+            x_ft[:, :, : self.modes1], self.weights1
+        )
+
+        # Return to physical space
+        x = torch.fft.irfft(out_ft, n=x.size(-1))
+
+        x = self.batch_norm1(x, time_delta)
+
+        return x
---
+++ src/model.py
@@ -0,0 +1,95 @@
+import torch
+import torch.nn as nn
+
+from layers import ResidualBlock, SpectralConv1d
+
+
+class FNO1d(nn.Module):
+    def __init__(self, modes, width):
+        """
+        The overall network. It contains 4 layers of the Fourier layer.
+        1. Lift the input to the desire channel dimension by self.fc0 .
+        2. 4 layers of the integral operators u' = (W + K)(u).
+            W defined by self.w; K defined by self.conv .
+        3. Project from the channel space to the output space by self.fc1 and self.fc2 .
+        input: the solution of the initial condition and location (a(x), x)
+        input shape: (batchsize, x=s, c=2)
+        output: the solution of a later timestep
+        output shape: (batchsize, x=s, c=1)
+        """
+        super(FNO1d, self).__init__()
+        self.modes = modes
+        self.width = width
+        self.padding = 1  # pad the domain if input is non-periodic
+        self.last_layer_width = 32
+
+        self.linear_p = nn.Linear(
+            3, self.width
+        )  # input channel is 2: (u0(x), x, t) --> GRID IS INCLUDED!
+
+        self.spectral_layers = nn.ModuleList(
+            [SpectralConv1d(self.width, self.width, self.modes) for _ in range(4)]
+        )
+
+        self.linear_conv_layers = nn.ModuleList(
+            [ResidualBlock(self.width) for _ in range(4)]
+        )
+
+        self.linear_q = nn.Linear(self.width, self.last_layer_width)
+
+        self.output_layer = nn.Linear(self.last_layer_width, 1)
+
+        self.activation = torch.nn.Tanh()
+        self.last_activation = torch.nn.ReLU()
+        # self.dropout = nn.Dropout(0.2)
+
+    def fourier_layer(self, x, time_delta):
+        ##########################################
+        # TODO: Implement the Fourier layer:
+        ##########################################
+        for f, c in zip(self.spectral_layers, self.linear_conv_layers):
+            x = f(x, time_delta) + c(x, time_delta)
+            x = self.activation(x)
+            # x = self.dropout(x)
+        return x
+
+    def rescale(self, x: torch.Tensor):
+        x_v = x[..., 0]
+        scale = torch.max(torch.abs(x_v), dim=1).values
+
+        x_rescaled = x_v / scale.unsqueeze(-1)
+        x[..., :1] = x_rescaled.unsqueeze(-1)
+        return x, scale
+
+    def unscale(self, x: torch.Tensor, scale: torch.Tensor):
+        return x.squeeze(-1) * scale.unsqueeze(-1)
+
+    # def rescale(self, x: torch.Tensor):
+    #     x_v = x[..., 0]
+    #     min = torch.min(x_v, dim=1).values
+    #     max = torch.max(x_v, dim=1).values
+    #     x_rescaled = 2 * (x_v - min.unsqueeze(1)) / (max - min).unsqueeze(1) - 1
+    #     x[..., :1] = x_rescaled.unsqueeze(-1)
+    #     return x, min, max
+
+    # def unscale(self, x: torch.Tensor, max: torch.Tensor, min: torch.Tensor):
+    #     return (
+    #         (x + 1).squeeze(-1) * (max - min).unsqueeze(1) / 2 + min.unsqueeze(1)
+    #     ).unsqueeze(-1)
+
+    def forward(self, x: torch.Tensor, time_delta: torch.Tensor):
+        #################################################
+        # TODO: Implement the forward method
+        #        using the Fourier and the Linear layer:
+        #################################################
+        # swap the channel dimension to the last dimension
+        # x, scale = self.rescale(x)
+        x = self.linear_p(x)
+        x = x.permute(0, 2, 1)
+        x = self.fourier_layer(x, time_delta)
+        x = x.permute(0, 2, 1)
+        x = self.linear_q(x)
+        x = self.last_activation(x)
+        x = self.output_layer(x)
+        # x = self.unscale(x, scale)
+        return x
